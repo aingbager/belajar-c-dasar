63b60c4f283509ffffbc0656305372a7d6295b11
aingbager <aingbager55@gmail.com>
2023-07-23 11:42:55 +0700

    add variable

---
+++ variable/float/float1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  float a = 2.5;
+  float b = 4.55;
+
+  printf("%f\n", a);
+  printf("%f\n", b);
+}
---
+++ variable/integer/integer1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  // type data integer
+  int a = 1;
+  int b = 2;
+
+  printf("%d\n", a);
+  printf("%d\n", b); //%d type data bilangan bulat
+}
